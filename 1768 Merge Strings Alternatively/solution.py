ecfea735e1d31a5e64f96fdaeb2e9de248c85d00
David Nguyen <davido.nguyen1212@gmail.com>
2025-02-12 19:43:37 -0800

    1768 merge strings alternatively

---
+++ 1768 Merge Strings Alternatively/solution.py
@@ -0,0 +1,12 @@
+def mergeAlternately(word1: str, word2: str) -> str:
+    string = []
+    i = 0
+    while i < len(word1) and i < len(word2):
+        string.append(word1[i])
+        string.append(word2[i])
+        i += 1
+
+    string.append(word1[i:])
+    string.append(word2[i:])
+
+    return "".join(string)
