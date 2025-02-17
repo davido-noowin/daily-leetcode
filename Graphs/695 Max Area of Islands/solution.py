fab8b5d4e4e5118f0bf39d881fc32c9ddc3a3e2d
David Nguyen <davido.nguyen1212@gmail.com>
2025-02-16 22:15:31 -0800

    695 max area of island

---
+++ Graphs/695 Max Area of Islands/solution.py
@@ -0,0 +1,17 @@
+def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
+    ROWS = len(grid)
+    COLS = len(grid[0])
+
+    def dfs(i, j):
+        if i < 0 or i >= ROWS or j < 0 or j >= COLS or grid[i][j] == 0:
+            return 0
+        else:
+            grid[i][j] = 0
+            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+
+    max_area = 0
+    for row in range(ROWS):
+        for col in range(COLS):
+            if grid[row][col] == 1:
+                max_area = max(dfs(row, col), max_area)
+    return max_area
